172da8bbc1af9f9ada707acdb5ee90f1e2b9db01
jmo0612 <jimmyhardmondow@gmail.com>
2020-07-05 15:40:09 +0800

    starter with fw

--- app/src/main/java/com/thowo/bmd/MainActivity.java
+++ app/src/main/java/com/thowo/bmd/MainActivity.java
@@ -4,12 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.thowo.jmandroidframework.JMAnFunctions;
+import com.thowo.jmandroidframework.component.JMAnActivity;
+import com.thowo.jmandroidframework.component.JMAnAsyncLoaderDefault;
+import com.thowo.jmandroidframework.component.JMAnUIMessenger;
+import com.thowo.jmjavaframework.JMFunctions;
+
+public class MainActivity extends JMAnActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //jimi
+        init();
+    }
+    private void init(){
+        JMFunctions.setUIListener(new JMAnUIMessenger());
+        JMFunctions.setAsyncListener(new JMAnAsyncLoaderDefault(this));
+        //File languageExcelFile=JMAnFunctions.copyResourceToFileDir(R.raw.jmlanguagepack,"jmlanguagepack.xls",false);
+        JMAnFunctions.init();
     }
 }
