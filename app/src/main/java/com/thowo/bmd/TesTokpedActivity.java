26d269eb79a58188f221ef7427d113ccf236099c
jimi <jimmyhardmondow@gmail.com>
2020-12-27 23:50:55 +0800

    pause development

--- app/src/main/java/com/thowo/bmd/TesTokpedActivity.java
+++ app/src/main/java/com/thowo/bmd/TesTokpedActivity.java
@@ -3,8 +3,11 @@ package com.thowo.bmd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.thowo.jmandroidframework.component.JMAnActivity;
+import com.thowo.jmandroidframework.component.JMAnButtonCustomVertical;
+import com.thowo.jmjavaframework.JMFunctions;
 
 public class TesTokpedActivity extends JMAnActivity {
 
@@ -12,5 +15,21 @@ public class TesTokpedActivity extends JMAnActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tes_tokped);
+        JMAnButtonCustomVertical btn=(JMAnButtonCustomVertical)findViewById(R.id.btnCustomTes);
+        //btn.setEnabled(false);
+
+        btn.setMyOnClickListener(new Runnable() {
+            @Override
+            public void run() {
+                JMFunctions.trace("CLICKED");
+            }
+        });
+
+        btn.setMyOnLongClickListener(new Runnable() {
+            @Override
+            public void run() {
+                JMFunctions.trace("LONG CLICKED");
+            }
+        });
     }
 }
